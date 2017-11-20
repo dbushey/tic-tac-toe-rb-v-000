7fb46f4daf194a4a22d08adb45ac7f747ab02067
Daiane Bushey <daianebushey@gmail.com>
2017-11-20 23:37:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,3 +28,16 @@ end
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    return false
+
+  elsif index >= 0 && index <= 8
+    return true
+
+  else
+    return false
+
+  end
+end
