f29f0067d256c4fa31e273eab8d4f7562151d6bc
Daiane Bushey <daianebushey@gmail.com>
2017-11-20 23:45:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,6 +58,17 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+
+  board.each do |token|
+    if token != " "
+      counter +=1
+    end
+  end
+  return counter
+end
+
 def current_player(board)
   if turn_count(board) % 2 == 0
     return "X"
